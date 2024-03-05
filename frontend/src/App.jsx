e81080d73252780f550f05a18461ab9e14f0caf6
Sumitsh28 <sumit.off28@gmail.com>
2024-03-05 12:19:27 +0530

    UI completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Login from "./pages/login/Login";
+import videoBG from "../src/assets/bg.mp4";
+import SignUp from "./pages/signup/SignUp";
+import Home from "./pages/home/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="relative">
+      <video
+        src={videoBG}
+        autoPlay
+        loop
+        muted
+        className="absolute top-0 left-0 min-w-full min-h-full object-cover z-0 "
+      />
+      <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
+      <div className="p-4 h-screen flex items-center justify-center relative z-10">
+        <Home />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
